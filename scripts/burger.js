1928aec2d4aeebf48e5de59584ee973cb073af15
Wadimx199789 <martinkewichwadim@gmail.com>
2022-06-12 14:16:56 +0300

    fix: change about page

--- scripts/burger.js
+++ scripts/burger.js
@@ -16,35 +16,5 @@ burgerButton.addEventListener("click",toggle);
 
 
 
-const countSuppliers = document.querySelector(".count-suppliers");
-const countCountries = document.querySelector(".count-countries");
-const countRetail = document.querySelector(".countRetail");
-const countHoreca = document.querySelector(".countHoreca");
-
-
-// // Взять элемент, анимировать до 2000 за 2 секунды
-// animate({elem: span[0], to: 360, sec: 5});
-// animate({elem: span[1], to: 83, sec: 5});
-// animate({elem: span[2], to: 100, sec: 5});
-animate({elem: countSuppliers, to:70, sec:2});
-animate({elem: countCountries, to:17, sec:2});
-animate({elem: countRetail, to:50, sec:5});
-animate({elem: countHoreca, to:40, sec:5});
-
-function animate({elem, from = 0, to, sec}) {
-  let tick = (to - from) / (60 * sec);
-  loop();
-  function loop() {
-    elem.textContent = Math.round( from += tick );
-    
-    if( from >= to ) {
-      // Прервать, как только дойдет до конца.
-      elem.textContent = to;
-      return;
-    }
-    
-    requestAnimationFrame(loop);
-  }
-}
 
 // // console.log(countCountries);
--- scripts/counter.js
+++ scripts/counter.js
@@ -1,14 +1,20 @@
-const span = document.querySelectorAll(".counter");
+
 const countSuppliers = document.querySelector(".count-suppliers");
 const countCountries = document.querySelector(".count-countries");
+const countRetail = document.querySelector(".countRetail");
+const countHoreca = document.querySelector(".countHoreca");
+const countBrends = document.querySelector(".count-brends");
+const countYears = document.querySelector(".count-years");
+const countProducers = document.querySelector(".count-produsers");
 
 
-// Взять элемент, анимировать до 2000 за 2 секунды
-animate({elem: span[0], to: 360, sec: 5});
-animate({elem: span[1], to: 83, sec: 5});
-animate({elem: span[2], to: 100, sec: 5});
-animate({elem: countSuppliers, to:70, sec:5});
-animate({elem: countCountries, to:17, sec:5});
+animate({elem: countBrends, to: 360, sec: 5});
+animate({elem: countYears, to: 360, sec: 5});
+animate({elem: countProducers, to: 360, sec: 5});
+animate({elem: countSuppliers, to:70, sec:2});
+animate({elem: countCountries, to:17, sec:2});
+animate({elem: countRetail, to:50, sec:5});
+animate({elem: countHoreca, to:40, sec:5});
 
 function animate({elem, from = 0, to, sec}) {
   let tick = (to - from) / (60 * sec);
@@ -25,3 +31,4 @@ function animate({elem, from = 0, to, sec}) {
     requestAnimationFrame(loop);
   }
 }
+
